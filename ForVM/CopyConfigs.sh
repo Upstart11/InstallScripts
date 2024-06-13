c155d6762b00a81b855ac7d0b42b2fb77eff11ed
Ciocoi Vladislav <vladislav.siloci1@gmail.com>
2024-06-13 13:21:40 +0300

    Adde commands to replace nanorc

--- ForVM/CopyConfigs.sh
+++ ForVM/CopyConfigs.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
+
+#Cloning the dotfiles repo
+
+git clone https://github.com/Upstart11/dotFiles.git
+
+sudo rm -f ~/.nanorc > /dev/null 2>&1
+ln -s ~/dotFiles/LinuxCloud/nano/nanorc ~/.nanorc
+
+
+#creating crontab job
 (crontab -l 2>/dev/null; echo "1 * * * * /usr/local/bin/clear_cache.sh") | crontab -

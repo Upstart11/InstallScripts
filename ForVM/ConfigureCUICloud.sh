6db65be531d3ba0685356667dc526bb212d05838
Ciocoi Vladislav <vladislav.siloci1@gmail.com>
2024-06-26 13:28:45 +0300

    Added websocket too

--- ForVM/ConfigureCUICloud.sh
+++ ForVM/ConfigureCUICloud.sh
@@ -28,7 +28,7 @@ sudo apt-get --assume-yes install micro
 git clone https://github.com/Upstart11/Utils.git
 git clone https://github.com/Upstart11/APIRetranslators.git
 
-pip install fastapi uvicorn
+pip install fastapi uvicorn websocket
 
 chmod +x Utils/AutoClrC.sh
 chmod +x Utils/ClrC.sh
